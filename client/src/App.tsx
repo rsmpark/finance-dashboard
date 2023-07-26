5cc4607d66ae709cdd613986c26a55fc53052022
Rick Park <ricksmp95@gmail.com>
2023-07-26 11:02:44 +0100

    Move files to client folder.

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  const { data1, data2, data3, data4, data5, data6 } = data;
+  const test = {
+    test: "abc",
+    test: "abc",
+    test: "abc",
+    test: "abc",
+    test: "abc",
+    test: "abc",
+    test: "abc",
+    test: "abc",
+  };
+  return <div>test</div>;
+}
+
+export default App;
