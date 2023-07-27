9f46f1be12ffda1a7f66d162e65e00fa42d5507e
Rick Park <ricksmp95@gmail.com>
2023-07-27 15:04:47 +0100

    Add RTK query setup and required response types.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,21 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 import App from "@/App";
+import { api } from "@/state/api";
 
 import "@/index.css";
 
+export const store = configureStore({
+  reducer: { [api.reducerPath]: api.reducer },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
